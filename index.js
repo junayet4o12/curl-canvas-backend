8f23d16810b1efdbd34df1d90cde40ece8069aa4
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-22 21:09:56 +0600

    service data api has made

--- index.js
+++ index.js
@@ -11,8 +11,34 @@ app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fbi4wg4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const curlCanvas = client.db('curlCanvas')
+        const servicesCollection = curlCanvas.collection('services')
+        // services start 
+        app.get('/services', async (req, res) => {
+            const result = await servicesCollection.find().toArray();
+            res.send(result)
+        })
+        // services end
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
