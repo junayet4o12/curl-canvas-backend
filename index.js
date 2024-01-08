c74c8c7485f6903f45642c3544d0014e799f4595
junayet <muhammadjunayetmaruf@gmail.com>
2024-01-08 13:01:10 +0600

    service can be registered and it stored in backend

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
         const portfolioCollection = curlCanvas.collection('portfolio')
         const barbersCollection = curlCanvas.collection('barbers')
         const usersCollection = curlCanvas.collection('users')
+        const registeredCollection = curlCanvas.collection('registered')
 
         // users start
         app.get('/users', async (req, res) => {
@@ -50,6 +51,12 @@ async function run() {
         // services start 
         app.get('/services', async (req, res) => {
             const result = await servicesCollection.find().toArray();
+            res.send(result)
+        })
+        app.get('/service/:id', async (req, res) => {
+            const id = req?.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await servicesCollection.findOne(query)
             res.send(result)
         })
         app.get('/servicesamount', async (req, res) => {
@@ -77,7 +84,39 @@ async function run() {
             const result = await barbersCollection.find().toArray();
             res.send(result)
         })
+        app.get('/barbers/:id', async (req, res) => {
+            const query = {_id: new ObjectId(req?.params?.id)}
+            const result = await barbersCollection.findOne(query);
+            res.send(result)
+        })
         // barbers end
+        // registeredCollection start
+        app.get('/register', async (req, res) => {
+            const result = await registeredCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/register', async (req, res) => {
+            const registrationData = req.body;
+            const query1 = {_id: new ObjectId(registrationData?.barberId)}
+            const query2 = {_id: new ObjectId(registrationData?.serviceId)}
+            console.log(registrationData, query1, query2);
+            const updatedData1 = {
+                $inc: {
+                    workCompleted: 1 
+                }
+            }
+            const updatedData2 = {
+                $push: {
+                    booked: registrationData?.serviceDate 
+                }
+            }
+            const result1 = await  registeredCollection.insertOne(registrationData)
+            const result2 = await barbersCollection.updateOne(query1, updatedData1)
+            const result3 = await servicesCollection.updateOne(query2, updatedData2)
+           
+            res.send({result1, result2, result3})
+        })
+        // registeredCollection end
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
